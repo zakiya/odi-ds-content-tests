ac43551d65605d5edeeddcb7c38d12482418d71d
Zakiya <zakiya.khabir@digital.ca.gov>
2022-03-17 15:26:29 -0700

    Consolidate code.

--- component-audit.js
+++ component-audit.js
@@ -3,36 +3,43 @@ const fs = require("fs");
 
 const demo = {
   directoryPath: path.join(__dirname, "node_modules/@cagov/"),
-  demoContents: "",
   template: "/template.html",
-  createDemoContents: (file) => {
-    const contents = fs.readFileSync(file);
-    console.log(contents);
-    return contents;
-  },
-  writeDemo: (component, file) => {
+  writeEach: (component, file) => {
+    // Define the destination file.
     const destinationFile = `demo/${component}.html`;
-    const code = demo.createDemoContents(file);
+
+    // Put the page together.
+    let code = `<link rel="stylesheet" href="dist/styles.css">\n`;
+    code += `<script type="module" src='dist/scripts.js'></script>\n`;
+    code += fs.readFileSync(file);
+
+    // Write file.
     fs.writeFile(destinationFile, code, (error) => {
       if (error) {
         return console.log(error);
       }
       return console.log(`Creating file ${destinationFile}.`);
     });
+
+    // Copy css.
+    // Copy js.
+  },
+  writeAll: () => {
+    // Read components directory.
+    fs.readdir(demo.directoryPath, (err, directories) => {
+      // Catch error.
+      if (err) {
+        console.log(`Unable to scan directory: ${err}`);
+      }
+
+      directories.forEach((component) => {
+        const file = `${demo.directoryPath}${component}${demo.template}`;
+        if (fs.existsSync(file)) {
+          demo.writeEach(component, file);
+        }
+      });
+    });
   }
 };
 
-// Read components directory.
-fs.readdir(demo.directoryPath, (err, directories) => {
-  // Catch error.
-  if (err) {
-    demo.demoContents = `Unable to scan directory: ${err}`;
-  }
-
-  directories.forEach((component) => {
-    const file = `${demo.directoryPath}${component}${demo.template}`;
-    if (fs.existsSync(file)) {
-      demo.writeDemo(component, file);
-    }
-  });
-});
+demo.writeAll();
