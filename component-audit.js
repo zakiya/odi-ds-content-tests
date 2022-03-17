2047f2463f08c59b26c3f46de4802bd22ba78965
Zakiya <zakiya.khabir@digital.ca.gov>
2022-03-17 15:44:53 -0700

    Generate index file.

--- component-audit.js
+++ component-audit.js
@@ -4,6 +4,11 @@ const fs = require("fs");
 const demo = {
   directoryPath: path.join(__dirname, "node_modules/@cagov/"),
   template: "/template.html",
+  indexFile: "demo/index.html",
+  indexCode: "",
+  makeIndex: (component) => {
+    demo.indexCode += `<p><a href="${component}.html">${component}</a></p>\n`;
+  },
   writeEach: (component, file) => {
     // Define the destination file.
     const destinationFile = `demo/${component}.html`;
@@ -36,7 +41,15 @@ const demo = {
         const file = `${demo.directoryPath}${component}${demo.template}`;
         if (fs.existsSync(file)) {
           demo.writeEach(component, file);
+          demo.makeIndex(component);
         }
+      });
+
+      fs.writeFile(demo.indexFile, demo.indexCode, (error) => {
+        if (error) {
+          return console.log(error);
+        }
+        return console.log(`Creating file ${demo.indexFile}.`);
       });
     });
   }
