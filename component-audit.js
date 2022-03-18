5d5e92a79724b075c2d62bb51847f894b8b708e0
Zakiya <zakiya.khabir@digital.ca.gov>
2022-03-17 17:16:12 -0700

    Refactor.

--- component-audit.js
+++ component-audit.js
@@ -1,48 +1,17 @@
 const path = require("path");
 const fs = require("fs");
 
-const demo = {
-  directoryPath: path.join(__dirname, "node_modules/@cagov/"),
-  template: "/template.html",
-  indexFile: "demo/index.html",
-  indexCode: "",
-  makeIndex: (component) => {
-    demo.indexCode += `<p><a href="${component}.html">${component}</a></p>\n`;
-  },
-  writeCSS: (component) => {
-    let code = ``;
-    try {
-      const css1 = `${demo.directoryPath}${component}/src/css/index.css`;
-      const css2 = `${demo.directoryPath}${component}/index.css`;
-      const css3 = `${demo.directoryPath}${component}/src/index.css`;
-      const css4 = `${demo.directoryPath}${component}/dist/index.css`;
-      let cssFile = "";
+const vars = {
+  directoryPath: path.join(__dirname, "node_modules/@cagov/")
+};
 
-      if (fs.existsSync(css1)) {
-        cssFile = css1;
-      } else if (fs.existsSync(css2)) {
-        cssFile = css2;
-      } else if (fs.existsSync(css3)) {
-        cssFile = css3;
-      } else if (fs.existsSync(css4)) {
-        cssFile = css4;
-      }
-      code += `<style type="text/css">\n`;
-      code += fs.readFileSync(cssFile);
-      code += `</style>\n`;
-    } catch (err) {
-      console.log(err);
-      code = "";
-    }
-
-    return code;
-  },
+const eachComponent = {
   writeEach: (component, templateFile) => {
     // Define the destination file.
     const destinationFile = `demo/${component}.html`;
 
     // Put the page together.
-    let code = demo.writeCSS(component);
+    let code = eachComponent.writeCSS(component);
     code += `<script type="module" src='dist/scripts.js'></script>\n`;
     code += fs.readFileSync(templateFile);
 
@@ -54,21 +23,54 @@ const demo = {
       return console.log(`Creating file ${destinationFile}.`);
     });
 
-    // Copy css.
     // Copy js.
+  },
+  writeCSS: (component) => {
+    let code = ``;
+    const componentPath = vars.directoryPath + component;
+    const index = "/index.css";
+    const pathsToTry = ["/src/css/", "/", "/src/", "/dist/"];
+
+    try {
+      let cssFile = "";
+
+      pathsToTry.forEach((cssPath) => {
+        if (fs.existsSync(componentPath + cssPath + index)) {
+          cssFile = componentPath + cssPath + index;
+        }
+      });
+
+      code += `<style type="text/css">\n`;
+      code += fs.readFileSync(cssFile);
+      code += `</style>\n`;
+      console.log(cssFile);
+    } catch (err) {
+      code = "";
+    }
+
+    return code;
+  }
+};
+
+const demo = {
+  template: "/template.html",
+  indexFile: "demo/index.html",
+  indexCode: "",
+  makeIndex: (component) => {
+    demo.indexCode += `<p><a href="${component}.html">${component}</a></p>\n`;
   },
   writeAll: () => {
     // Read components directory.
-    fs.readdir(demo.directoryPath, (err, directories) => {
+    fs.readdir(vars.directoryPath, (err, directories) => {
       // Catch error.
       if (err) {
         console.log(`Unable to scan directory: ${err}`);
       }
 
       directories.forEach((component) => {
-        const templateFile = `${demo.directoryPath}${component}${demo.template}`;
+        const templateFile = `${vars.directoryPath}${component}${demo.template}`;
         if (fs.existsSync(templateFile)) {
-          demo.writeEach(component, templateFile);
+          eachComponent.writeEach(component, templateFile);
           demo.makeIndex(component);
         }
       });
